3dffb797554e169b852a40898cfce1220651d5f9
KendrickLiang <kliang5@stuy.edu>
2018-10-18 14:01:06 -0400

    made new database

---
+++ story.py
@@ -0,0 +1,27 @@
+import sqlite3 
+DB_FILE="story.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#fill given table with values
+       # command = "INSERT INTO %s Values (\'%s\',%s,%s)" % (tablename, row[0], row[1], row[2])
+       # c.execute(command) #run command
+
+#========================================================
+                 
+
+
+#build SQL stmt, save as string
+command = "CREATE TABLE users (username TEXT, password TEXT)"
+c.execute(command)
+
+command = "CREATE TABLE stories (id INTEGER, story name TEXT)"
+c.execute(command)
+
+command = "CREATE TABLE placeholder (entry INTEGER, content TEXT, users TEXT, timestamp TEXT)"
+c.execute(command)
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
